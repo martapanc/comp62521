495bd008510f296370807790acd53b05bbb8f1cd
Marta <marta.panc@gmail.com>
2017-11-12 18:47:39 +0000

    Changed degault table entries to 25, improved countauthors.py

--- src/comp62521/statistics/author_count.py
+++ src/comp62521/statistics/author_count.py
@@ -1,12 +1,10 @@
 def appearing_first(name, author_list):
-    t = 0
     if name == author_list[0]:
-        t = 1
-    return t
+        return 1
+    return 0
 
 def appearing_last(name, author_list):
-    t = 0
     tmp_len = len(author_list)
-    if name == author_list[tmp_len - 1]:
-        t = 1
-    return t
+    if name == author_list[-1]:
+        return 1
+    return 0
