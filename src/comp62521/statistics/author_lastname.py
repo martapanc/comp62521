09843ff46f0ecf4c424a020142d7d0030c9195a7
Marta <marta.panc@gmail.com>
2017-11-16 20:55:23 +0000

    Fixed error in author names in database containing numbers

--- src/comp62521/statistics/author_lastname.py
+++ src/comp62521/statistics/author_lastname.py
@@ -9,6 +9,9 @@ def get_last_name_first(author):
     if author != "":
         author_items = author.split()
         first_names = ","
+        for item in author_items: #Check the author name does not contain extraneous characters (like numbers)
+            if item.isdigit():
+                author_items.remove(item)
         if len(author_items) > 1:
             for item in author_items[:-1]:
                 first_names += " " + item
