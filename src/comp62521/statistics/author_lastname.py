a27efcdad81c5688ae38d4ae670ac70be03a289d
Marta <marta.panc@gmail.com>
2017-11-16 16:15:21 +0000

    TDD: fixed case with empty author name

--- src/comp62521/statistics/author_lastname.py
+++ src/comp62521/statistics/author_lastname.py
@@ -6,11 +6,13 @@ def get_author_last_name(author):
     return -1
 
 def get_last_name_first(author):
-    author_items = author.split()
-    first_names = ","
-    if len(author_items) > 1:
-        for item in author_items[:-1]:
-            first_names += " " + item
-        return author_items[-1] + first_names
-    else:
-        return author_items[-1]
+    if author != "":
+        author_items = author.split()
+        first_names = ","
+        if len(author_items) > 1:
+            for item in author_items[:-1]:
+                first_names += " " + item
+            return author_items[-1] + first_names
+        else:
+            return author_items[-1]
+    return -1
