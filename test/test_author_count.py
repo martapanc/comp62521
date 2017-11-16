c671f5e28c1e35d5031f0ab42431bee457f1c499
zkk-PC\zkk <1021378794@qq.com>
2017-11-16 14:33:21 +0000

    fix the error

--- test/test_author_count.py
+++ test/test_author_count.py
@@ -11,13 +11,13 @@ class TestAverage(unittest.TestCase):
         self.assertEqual(author_count.appearing_first("oguz", ["oguz"]), 1)
         self.assertEqual(author_count.appearing_first("oguz", ["oguz", "a"]), 1)
         self.assertEqual(author_count.appearing_first("oguz", ["b", "oguz", "a"]), 0)
-        self.assertEqual(author_count_appearing_first("oguz",["a","b","c"]),0)
+        self.assertEqual(author_count.appearing_first("oguz",["a","b","c"]),0)
 
     def test_last_appearance(self):
         self.assertEqual(author_count.appearing_last("oguz", ["oguz"]), 1)
         self.assertEqual(author_count.appearing_last("oguz", ["a", "oguz"]), 1)
         self.assertEqual(author_count.appearing_last("oguz", ["a", "oguz", "b"]), 0)
-        self.assertEqual(author_count_appearing_last("oguz",["a","b","c"]),0)
+        self.assertEqual(author_count.appearing_last("oguz",["a","b","c"]),0)
 
 if __name__ == '__main__':
     unittest.main()
