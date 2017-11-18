9e77523eb0116b4b3b547364407306446f654802
Xianghe <xianghe.ji@postgrad.manchester.ac.uk>
2017-11-18 22:26:40 +0000

    fixed error and added wrong author name test

--- test/test_author_stats.py
+++ test/test_author_stats.py
@@ -6,11 +6,15 @@ from comp62521.database import database
 class TestAuthorStats(unittest.TestCase):
 
     def setUp(self):
-        pass
+        dir, _ = path.split(__file__)
+        self.data_dir = path.join(dir, "..", "data")
 
     def test_author_stats(self):
-        self.assertEqual(author_stats.get_author_stats('Stefano Ceri'),(218, 100, 94, 6, 18, 230, 86, 33))
-
+        db = database.Database()
+        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
+        self.assertEqual(db.get_author_stats('Stefano Ceri'),(218, 100, 94, 6, 18, 230, 86, 33))
+        self.assertEqual(db.get_author_stats(''), (0, 0, 0, 0, 0, 0, 0, 0))
+        self.assertEqual(db.get_author_stats('Xianghe'), (0, 0, 0, 0, 0, 0, 0, 0))
 
 
 if __name__ == '__main__':
